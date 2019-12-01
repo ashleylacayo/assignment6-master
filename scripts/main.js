fc39a60d50328cabc8979276cccc3568c07e2c8a
Ashley Lacayo <ashleylacayo@ufl.edu>
2019-12-01 17:45:51 -0500

    Java created and edited

---
+++ scripts/main.js
@@ -0,0 +1,22 @@
+var members=[
+  'Dave Grohl',
+  'Nate Mendel',
+  'Pat Smear',
+  'Taylor Hawkins',
+  'Rami Jaffee',
+  'Chris Shiflett'
+];
+
+var tour ={
+  chicago: "July 29-30",
+  seattle: "September 1",
+  Edmonton: "September 4"
+};
+
+var i = 0
+
+while (i < 5) {
+  console.log('Times running through the loop x' + i);
+
+  i++;
+}
